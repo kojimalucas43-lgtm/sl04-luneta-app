c380e201df17fa7cf85c1da3c41287bb87843e7c
Kojima Lucas <kojimalucas43@gmail.com>
2026-08-13 07:57:34 -0300

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+ export default function Home(){
+    return(
+        <View>
+            <Text>INDEX</Text>
+        </View>
+    )
+ }
+
+ const s = StyleSheet.create({
+
+ })
